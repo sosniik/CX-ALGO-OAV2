90fd72fee5628b5a6131b786cfe945b70facfa67
Leo Lemercier <leolemercier@MBP-de-Leo-2.home>
2020-10-19 10:50:39 +0200

    exo1

---
+++ ex01.js
@@ -0,0 +1,18 @@
+function tri(tab){
+    var changed;
+    do{
+        changed = false;
+        for(var i=0; i < tab.length-1; i++) {
+            if(tab[i] > tab[i+1]) {
+                var tmp = tab[i];
+                tab[i] = tab[i+1];
+                tab[i+1] = tmp;
+                changed = true;
+            }
+        }
+    } while(changed);
+}
+
+var tab = [6, 9, 3, 15, 2, 17, 1];
+tri(tab);
+console.log(tab);
---
+++ ex02.js
@@ -0,0 +1,11 @@
+function tri_insertion(tableau){
+    console.log( )
+    n = tableau.length
+    console.log(n)
+}
+
+var tableau = [6, 5, 3, 1, 8, 7, 2, 4];
+tri_insertion(tableau);
+console.log(tableau)
+
+//[6, 5, 3, 1, 8, 7, 2, 4]
